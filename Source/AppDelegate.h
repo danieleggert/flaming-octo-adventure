1fc2a4408076a1ccdc023bc510a3de2a7234fece
Daniel Eggert <daniel.eggert@hoerbuch.dk>
2013-04-10 22:29:16 +0200

    Initial project structure.

---
+++ Source/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AwesomeCodeChallenge
+//
+//  Created by Daniel Eggert on 10/04/2013.
+//  Copyright (c) 2013 Bödewadt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
